5427e3776009ec81fdfdc1565ee5ef702dc69d7c
JAGDISH0008 <jagadeesh26597@gmail.com>
2024-03-16 15:33:13 -0700

    feat: declared the header and footer and modified the appcomponent to wrap the content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreeComponent } from './components/tree/tree.component';
 import { DialogComponent } from './components/dialog/dialog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TreeComponent,
-    DialogComponent
+    DialogComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

f6dbf5c8f5a16d1e496756ac1852b876c1c76594
JAGDISH0008 <jagadeesh26597@gmail.com>
2024-03-16 15:00:22 -0700

    feat: added the sample data.

---
+++ src/data/data.ts
@@ -0,0 +1,38 @@
+
+export const data = [{
+  "name": "root",
+  "children": [
+    {
+      "name": "child1",
+      "children": [
+        {
+          "name": "child1.1",
+        },
+        {
+          "name": "child1.2",
+        }
+      ]
+    },
+    {
+      "name": "child2",
+    }
+  ]
+}, {
+  "name": "root2",
+  "children": [
+    {
+      "name": "child1",
+      "children": [
+        {
+          "name": "child1.1",
+        },
+        {
+          "name": "child1.2",
+        }
+      ]
+    },
+    {
+      "name": "child2",
+    }
+  ]
+}];

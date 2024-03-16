8bdc061c54df215d7054166954d1178009f9da31
JAGDISH0008 <jagadeesh26597@gmail.com>
2024-03-16 15:09:00 -0700

    declared the dialog component and integrated the dialog component in to the tree component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreeComponent } from './components/tree/tree.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeComponent
+    TreeComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -10,6 +10,8 @@ export class TreeComponent implements OnInit {
   @Input()
   tree: Node[] = [];
   expandedStates: { [key: string]: boolean } = {};
+  isModalOpen = false;
+  selectedNodeName: string = '';
   constructor() {
   }
   ngOnInit(): void {
@@ -20,5 +22,9 @@ export class TreeComponent implements OnInit {
 
   isExpanded(nodeName: string): boolean {
     return !!this.expandedStates[nodeName];
+  }
+  openDialog(nodeName: string): void {
+    this.selectedNodeName = nodeName;
+    this.isModalOpen = true;
   }
 }
